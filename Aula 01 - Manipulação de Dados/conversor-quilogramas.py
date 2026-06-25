e4a22a9a40ebec4123831f2f3b1f98600b14e559
emilymrocha-ship-it <emily.m.rocha@edu.senai.br>
2026-06-25 20:56:09 -0300

    add: adiciona primeiro arquivo em python

--- Aula 01 - Manipulação de Dados/conversor-quilogramas.py
+++ Aula 01 - Manipulação de Dados/conversor-quilogramas.py
@@ -1,8 +1,5 @@
-# Solicita o valor em quilogramas
 quilogramas = float(input("Digite o valor em quilogramas: "))
 
-# Converte para gramas
 gramas = quilogramas * 1000
 
-# Exibe o resultado
 print(f"{quilogramas} kg equivalem a {gramas} g.")
